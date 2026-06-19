a45394596d02875c2a53b6889c9fa1e7d2cc62c8
Francesco <francescoforesti00@gmail.com>
2026-06-19 18:27:05 +0200

    fixed hedge

--- FixedHedge.py
+++ FixedHedge.py
@@ -1,39 +1,40 @@
+import math
 import numpy as np
 
 def algo():
     print('Configure Hedge Algorithm\n')
+    print('Select losses type:\n1)Stochastic_losses\n2)Adversarial_losses\n3)Low-gap_losses\n')
+    loss_choice = int(input())
     #print('Select learning rate\n')
-    #learning_rate = float(input())
+    learning_rate = 0.5#float(input())
     print('Enter the number K of desired experts\n')
     k =  int(input())
+    print('Enter the number of turns T\n')
+    T = int(input())
+
     #weights initialization
     index_selection = [i for i in range(k)]
     weights = [1 for i in range(k)]
     probabilities = [0 for i in range(k)]
-
-
-    print('Enter the number of turns T\n')
-    T = int(input())
-
     losses = [[]]
-    losses = np.loadtxt('Stochastic_losses.txt', usecols=range(k))
+    losses = np.loadtxt((loss_choice == 1)*'Stochastic_losses.txt' + (loss_choice == 2)*'Adversarial_losses.txt' + (loss_choice == 3)*'Low-gap_losses.txt', usecols=range(k))
 
-    print(losses)
-'''
+
+
     for t in range(T):
         somma = 0
-        #calcola somma
+        #define the distribution p
         for i in range(k):
             somma = somma + weights[i]
-        #calcola le probabilità
         for i in range(k):
             probabilities[i] = weights[i]/somma
-        #scegli I
-        choosen_index = np.random.choice(index_selection,probabilities)
-        #recupera le loss
 
+        #draw I according to p
+        chosen = np.random.choice(index_selection, 1 ,probabilities)
+        print(chosen)
 
+        #update the weights
         for i in range(k):
-            losses[i] =
-        #aggiorna weights
-        '''
+            weights[i] = weights[i]*math.exp((learning_rate*losses[t][i]))
+
+        print(weights)
